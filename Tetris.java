f4fe519a3b7c6a8da70cfd338e5ebc15a9289894
muskan kataria <47684499+muskan-kataria@users.noreply.github.com>
2019-07-19 18:24:02 +0530

    fixing of shape on screen and deletion of row

--- Tetris.java
+++ Tetris.java
@@ -2,28 +2,79 @@ import java.util.*;
 
 class Board
 {
-char[][] screen=new char[40][40];
+   public static  int max_rows=20;
+  public static  int max_cols=20;
+public static char[][] screen=new char[20][20];
+    public static int[] flag=new int[20];
+ 
+    
+    
+    Board()
+    {
+int i,j;
+        for(i=0;i<20;i++)
+        {
+        flag[i]=0;
+        }
+}
+
 
 void generate_board(){
 int i,j;
 
-for(i=0;i<40;i++)
-{for(j=0;j<40;j++)
-{if(j==39||j==0)
-screen[i][j]='|';
+for(i=0;i<max_rows;i++)
+{for(j=0;j<max_cols;j++)
+{
  
- else if(i==0||i==39)
-     screen[i][j]='-';
+    if(j==max_cols-1||j==0)
+screen[i][j]='*';
  
- else 
-     screen[i][j]=' ';
+ else if(i==0||i==max_rows-1)
+     screen[i][j]='*';
+}
+}
+}
+    
+    
+public static void display()
+{
+int i,j;
+
+for(i=0;i<max_rows;i++)
+{for(j=0;j<max_cols;j++)
+{System.out.print(screen[i][j]);
 
 }
+System.out.println();
 }
 }
+    
+    
+   public static  void delete_row()
+    {
+    
+        int i,j;
+for(i=0;i<20;i++)
+{
+    if(flag[i]==18)
+    {
+        flag[i]=0;
+        for(j=i;j>1;j--)
+        { screen[j]=screen[j-1];
+         flag[j]=flag[j-1];
+        }
+        
+        screen[0][0]='*';
+        screen[0][19]='*';
+        
+    }
+}
+        
+    }
+
 }
 
-class Point
+class Point 
 {
     int x=1;
     int y=1;
@@ -39,21 +90,82 @@ class Point
     }
 }
 
-class Shapes extends Board
+class Shapes 
 {
 int version;
+    int max_cols=20;
+    int max_rows=20;
 
 Point shape[];
 
 Shapes()
 {
 shape=new Point[4];
+
+
 }
 
 void cordinates()
 {
 
 }
+    int update_flag()
+    {
+        
+
+      Board.flag[shape[0].x]+=1;
+       Board.flag[shape[1].x]+=1;
+         Board.flag[shape[2].x]+=1;
+         Board.flag[shape[3].x]+=1;
+        
+        if(Board.flag[1]==1)
+            return 1;
+        
+        Board.delete_row();
+        
+        return 0;
+    }
+
+int check_bottom()
+{
+   
+    
+    if(shape[0].x==max_rows-1||shape[1].x==max_rows-2||shape[2].x==max_rows-2||shape[3].x==max_rows-2)
+        return 1;
+   
+  else if(Board.screen[shape[0].x+1][shape[0].y]=='#'||Board.screen[shape[1].x+1][shape[1].y]=='#'||Board.screen[shape[2].x+1][shape[2].y]=='#'||Board.screen[shape[3].x+1][shape[3].y]=='#')
+        return 1;
+    
+    else
+        return 0;
+}
+    
+    int check_left()
+{
+   
+    
+   
+   
+ if(Board.screen[shape[0].x][shape[0].y-1]=='#'||Board.screen[shape[1].x][shape[1].y-1]=='#'||Board.screen[shape[2].x][shape[2].y-1]=='#'||Board.screen[shape[3].x][shape[3].y-1]=='#')
+        return 1;
+    
+    else
+        return 0;
+}
+    
+        int check_right()
+{
+   
+    
+   
+   
+ if(Board.screen[shape[0].x][shape[0].y+1]=='#'||Board.screen[shape[1].x][shape[1].y+1]=='#'||Board.screen[shape[2].x][shape[2].y+1]=='#'||Board.screen[shape[3].x][shape[3].y+1]=='#')
+        return 1;
+    
+    else
+        return 0;
+}
+    
 
 void select_version(){}
 
@@ -76,7 +188,7 @@ shape[3].y=shape[3].y-1;
 void right(){
 
 
-if(shape[0].y==38||shape[1].y==38||shape[2].y==38||shape[3].y==38)
+if(shape[0].y==18||shape[1].y==18||shape[2].y==18||shape[3].y==18)
 return;
 
 shape[0].y=shape[0].y+1;
@@ -92,7 +204,7 @@ shape[3].y=shape[3].y+1;
 void bottom(){
 
 
-if(shape[0].x==38||shape[1].x==38||shape[2].x==38||shape[3].x==38)
+if(shape[0].x==18||shape[1].x==18||shape[2].x==18||shape[3].x==18)
 return;
 
 shape[0].x=shape[0].x+1;
@@ -112,25 +224,36 @@ void generate_shape()
 {
 
 
-
-screen[shape[0].x][shape[0].y]='#';
-screen[shape[1].x][shape[1].y]='#';
-screen[shape[2].x][shape[2].y]='#';
-screen[shape[3].x][shape[3].y]='#';
+Board.screen[shape[0].x][shape[0].y]='#';
+Board.screen[shape[1].x][shape[1].y]='#';
+Board.screen[shape[2].x][shape[2].y]='#';
+Board.screen[shape[3].x][shape[3].y]='#';
 }
 
 
-
-void display()
+void remove_prevshape()
 {
-int i,j;
-for(i=0;i<40;i++)
-{for(j=0;j<40;j++)
-{System.out.print(screen[i][j]);
 
+
+Board.screen[shape[0].x][shape[0].y]=' ';
+Board.screen[shape[1].x][shape[1].y]=' ';
+Board.screen[shape[2].x][shape[2].y]=' ';
+Board.screen[shape[3].x][shape[3].y]=' ';
 }
-System.out.println();}
-}
+
+
+    
+    int check_boundary(int x,int y)
+    {
+       if(x==Board.max_rows-1||x==0)
+           return 0;
+        else if(y==Board.max_cols-1||y==0)
+            return 0;
+        else
+            return 1;
+    }
+
+
 }
 
 
@@ -148,12 +271,14 @@ void random_generate()
 {
 
 Random rand = new Random(); 
- int rand_int1 = rand.nextInt(40); 
+ int rand_int1 = rand.nextInt(max_cols-1); 
 col=rand_int1;
  if(col==0)
  {
 random_generate();
  }
+ 
+ 
 
  
 }
@@ -184,8 +309,26 @@ v1();
 
 void v1()
 {
+int t=0;
 
+if(check_boundary(shape[0].x,shape[0].y)==1)
+{
 
+if(check_boundary(shape[1].x+1,shape[1].y-1)==1)
+{
+
+if(check_boundary(shape[2].x+2,shape[2].y-2)==1)
+{
+if(check_boundary(shape[3].x+3,shape[3].y-3)==1)
+{
+t=1;
+}
+}
+}
+}
+
+if(t==1)
+{
 
 shape[0].x=shape[0].x;
 shape[0].y=shape[0].y;
@@ -198,12 +341,34 @@ shape[2].y=shape[2].y-2;
 
 shape[3].x=shape[3].x+3;
 shape[3].y=shape[3].y-3;
+}
+else
+version=2;
 
 }
 
 void v2()
 {
+int t=0;
 
+if(check_boundary(shape[0].x,shape[0].y)==1)
+{
+
+if(check_boundary(shape[1].x-1,shape[1].y+1)==1)
+{
+
+if(check_boundary(shape[2].x-2,shape[2].y+2)==1)
+{
+if(check_boundary(shape[3].x-3,shape[3].y+3)==1)
+{
+t=1;
+}
+}
+}
+}
+
+if(t==1)
+{
 
 shape[0].x=shape[0].x;
 shape[0].y=shape[0].y;
@@ -216,7 +381,9 @@ shape[2].y=shape[2].y+2;
 
 shape[3].x=shape[3].x-3;
 shape[3].y=shape[3].y+3;
-
+}
+else
+version=1;
 
 }
 
@@ -244,9 +411,9 @@ void random_generate()
 {
 
 Random rand = new Random(); 
- int rand_int1 = rand.nextInt(39); 
+ int rand_int1 = rand.nextInt(max_cols-1); 
  col=rand_int1;
- if(col==0||col==38)
+ if(col==0||col==max_cols-2)
  {
 random_generate();
  }
@@ -286,10 +453,10 @@ void random_generate()
 {
 
 Random rand = new Random(); 
- int rand_int1 = rand.nextInt(39); 
+ int rand_int1 = rand.nextInt(max_cols-1); 
  col=rand_int1;
  
- if(col==0||col==38)
+ if(col==0||col==max_cols-2)
  {
 random_generate();
  }
@@ -314,24 +481,28 @@ void select_version()
 if(version==1)
 {version=2;
 v2();
+return;
 }
 
 else if(version==2)
 {
 version=3;
 v3();
+return;
 }
 
 else if(version==3)
 {
 version=4;
 v4();
+return;
 }
 
 else if(version==4)
 {
 version=1;
 v1();
+return;
 }
 }
 
@@ -339,6 +510,27 @@ v1();
 void v1()
 {
 
+
+int t=0;
+
+if(check_boundary(shape[0].x-2,shape[0].y)==1)
+{
+
+if(check_boundary(shape[1].x-1,shape[1].y-1)==1)
+{
+
+if(check_boundary(shape[2].x,shape[2].y-2)==1)
+{
+if(check_boundary(shape[3].x+1,shape[3].y-1)==1)
+{
+t=1;
+}
+}
+}
+}
+
+if(t==1)
+{
 
 shape[0].x=shape[0].x-2;
 shape[0].y=shape[0].y;
@@ -352,7 +544,9 @@ shape[2].y=shape[2].y-2;
 shape[3].x=shape[3].x+1;
 shape[3].y=shape[3].y-1;
 
-
+}
+else
+version=4;
 
 
 }
@@ -360,6 +554,27 @@ shape[3].y=shape[3].y-1;
 void v2()
 {
 
+
+int t=0;
+
+if(check_boundary(shape[0].x,shape[0].y+2)==1)
+{
+
+if(check_boundary(shape[1].x-1,shape[1].y+1)==1)
+{
+
+if(check_boundary(shape[2].x-2,shape[2].y)==1)
+{
+if(check_boundary(shape[3].x-1,shape[3].y-1)==1)
+{
+t=1;
+}
+}
+}
+}
+
+if(t==1)
+{
 
 shape[0].x=shape[0].x;
 shape[0].y=shape[0].y+2;
@@ -373,7 +588,9 @@ shape[2].y=shape[2].y;
 shape[3].x=shape[3].x-1;
 shape[3].y=shape[3].y-1;
 
-
+}
+else
+version=1;
 
 
 
@@ -383,7 +600,26 @@ shape[3].y=shape[3].y-1;
 void v3()
 {
 
+int t=0;
 
+if(check_boundary(shape[0].x+2,shape[0].y)==1)
+{
+
+if(check_boundary(shape[1].x+1,shape[1].y+1)==1)
+{
+
+if(check_boundary(shape[2].x,shape[2].y+2)==1)
+{
+if(check_boundary(shape[3].x-1,shape[3].y+1)==1)
+{
+t=1;
+}
+}
+}
+}
+
+if(t==1)
+{
 
 shape[0].x=shape[0].x+2;
 shape[0].y=shape[0].y;
@@ -396,7 +632,9 @@ shape[2].y=shape[2].y+2;
 
 shape[3].x=shape[3].x-1;
 shape[3].y=shape[3].y+1;
-
+}
+else
+version=2;
 
 
 }
@@ -406,7 +644,26 @@ void v4()
 {
 
 
+int t=0;
 
+if(check_boundary(shape[0].x,shape[0].y-2)==1)
+{
+
+if(check_boundary(shape[1].x+1,shape[1].y-1)==1)
+{
+
+if(check_boundary(shape[2].x+2,shape[2].y)==1)
+{
+if(check_boundary(shape[3].x+1,shape[3].y+1)==1)
+{
+t=1;
+}
+}
+}
+}
+
+if(t==1)
+{
 
 shape[0].x=shape[0].x;
 shape[0].y=shape[0].y-2;
@@ -420,7 +677,9 @@ shape[2].y=shape[2].y;
 shape[3].x=shape[3].x+1;
 shape[3].y=shape[3].y+1;
 
-
+}
+else
+version=3;
 
 
 }
@@ -444,10 +703,10 @@ void random_generate()
 {
 
 Random rand = new Random(); 
- int rand_int1 = rand.nextInt(39); 
+ int rand_int1 = rand.nextInt(max_cols-3); 
  col=rand_int1;
  
- if(col==0||col==1||col==38)
+ if(col==0||col==1||col==max_cols-2)
  {
 random_generate();
  }
@@ -497,6 +756,27 @@ void v1()
 {
 
 
+int t=0;
+
+if(check_boundary(shape[0].x-1,shape[0].y)==1)
+{
+
+if(check_boundary(shape[1].x-1,shape[1].y-2)==1)
+{
+
+if(check_boundary(shape[2].x,shape[2].y-1)==1)
+{
+if(check_boundary(shape[3].x+1,shape[3].y)==1)
+{
+t=1;
+}
+}
+}
+}
+
+if(t==1)
+{
+
 shape[0].x=shape[0].x-1;
 shape[0].y=shape[0].y;
 
@@ -508,7 +788,10 @@ shape[2].y=shape[2].y-1;
 
 shape[3].x=shape[3].x+1;
 shape[3].y=shape[3].y;
+}
 
+else
+version=4;
 
 
 
@@ -517,6 +800,27 @@ shape[3].y=shape[3].y;
 void v2()
 {
 
+
+int t=0;
+
+if(check_boundary(shape[0].x+1,shape[0].y)==1)
+{
+
+if(check_boundary(shape[1].x-1,shape[1].y)==1)
+{
+
+if(check_boundary(shape[2].x,shape[2].y-1)==1)
+{
+if(check_boundary(shape[3].x+1,shape[3].y-2)==1)
+{
+t=1;
+}
+}
+}
+}
+
+if(t==1)
+{
 
 shape[0].x=shape[0].x+1;
 shape[0].y=shape[0].y;
@@ -530,7 +834,9 @@ shape[2].y=shape[2].y-1;
 shape[3].x=shape[3].x+1;
 shape[3].y=shape[3].y-2;
 
-
+}
+else
+version=1;
 
 
 
@@ -541,6 +847,26 @@ void v3()
 {
 
 
+int t=0;
+
+if(check_boundary(shape[0].x,shape[0].y)==1)
+{
+
+if(check_boundary(shape[1].x,shape[1].y)==1)
+{
+
+if(check_boundary(shape[2].x-1,shape[2].y+1)==1)
+{
+if(check_boundary(shape[3].x-2,shape[3].y+2)==1)
+{
+t=1;
+}
+}
+}
+}
+
+if(t==1)
+{
 
 shape[0].x=shape[0].x;
 shape[0].y=shape[0].y;
@@ -553,7 +879,9 @@ shape[2].y=shape[2].y+1;
 
 shape[3].x=shape[3].x-2;
 shape[3].y=shape[3].y+2;
-
+}
+else
+version=2;
 
 
 }
@@ -564,6 +892,26 @@ void v4()
 
 
 
+int t=0;
+
+if(check_boundary(shape[0].x,shape[0].y)==1)
+{
+
+if(check_boundary(shape[1].x+2,shape[1].y+2)==1)
+{
+
+if(check_boundary(shape[2].x+1,shape[2].y+1)==1)
+{
+if(check_boundary(shape[3].x,shape[3].y)==1)
+{
+t=1;
+}
+}
+}
+}
+
+if(t==1)
+{
 
 shape[0].x=shape[0].x;
 shape[0].y=shape[0].y;
@@ -576,14 +924,10 @@ shape[2].y=shape[2].y+1;
 
 shape[3].x=shape[3].x;
 shape[3].y=shape[3].y;
-
-
-
-
 }
-
-
-
+else
+version=3;
+}
 }
 
 
@@ -601,9 +945,9 @@ void random_generate()
 {
 
 Random rand = new Random(); 
- int rand_int1 = rand.nextInt(39); 
+ int rand_int1 = rand.nextInt(max_cols-1); 
  col=rand_int1;
- if(col==0||col==38)
+ if(col==0||col==max_cols-2)
  {
 random_generate();
  }
@@ -652,6 +996,26 @@ void v1()
 {
 
 
+int t=0;
+
+if(check_boundary(shape[0].x,shape[0].y)==1)
+{
+
+if(check_boundary(shape[1].x+1,shape[1].y-1)==1)
+{
+
+if(check_boundary(shape[2].x,shape[2].y)==1)
+{
+if(check_boundary(shape[3].x+1,shape[3].y-1)==1)
+{
+t=1;
+}
+}
+}
+}
+
+if(t==1)
+{
 shape[0].x=shape[0].x;
 shape[0].y=shape[0].y;
 
@@ -664,7 +1028,9 @@ shape[2].y=shape[2].y;
 shape[3].x=shape[3].x+1;
 shape[3].y=shape[3].y-1;
 
-
+}
+else
+version=4;
 
 
 }
@@ -672,6 +1038,26 @@ shape[3].y=shape[3].y-1;
 void v2()
 {
 
+int t=0;
+
+if(check_boundary(shape[0].x,shape[0].y+2)==1)
+{
+
+if(check_boundary(shape[1].x-1,shape[1].y+1)==1)
+{
+
+if(check_boundary(shape[2].x,shape[2].y)==1)
+{
+if(check_boundary(shape[3].x-1,shape[3].y-1)==1)
+{
+t=1;
+}
+}
+}
+}
+
+if(t==1)
+{
 
 shape[0].x=shape[0].x;
 shape[0].y=shape[0].y+2;
@@ -684,7 +1070,9 @@ shape[2].y=shape[2].y;
 
 shape[3].x=shape[3].x-1;
 shape[3].y=shape[3].y-1;
-
+}
+else
+version=1;
 
 
 
@@ -696,6 +1084,26 @@ void v3()
 {
 
 
+int t=0;
+
+if(check_boundary(shape[0].x+1,shape[0].y)==1)
+{
+
+if(check_boundary(shape[1].x,shape[1].y+1)==1)
+{
+
+if(check_boundary(shape[2].x,shape[2].y)==1)
+{
+if(check_boundary(shape[3].x+1,shape[3].y+1)==1)
+{
+t=1;
+}
+}
+}
+}
+
+if(t==1)
+{
 
 shape[0].x=shape[0].x+1;
 shape[0].y=shape[0].y;
@@ -708,7 +1116,9 @@ shape[2].y=shape[2].y;
 
 shape[3].x=shape[3].x+1;
 shape[3].y=shape[3].y+1;
-
+}
+else
+version=2;
 
 
 }
@@ -720,6 +1130,26 @@ void v4()
 
 
 
+int t=0;
+
+if(check_boundary(shape[0].x-1,shape[0].y-2)==1)
+{
+
+if(check_boundary(shape[1].x,shape[1].y-1)==1)
+{
+
+if(check_boundary(shape[2].x,shape[2].y)==1)
+{
+if(check_boundary(shape[3].x-1,shape[3].y+1)==1)
+{
+t=1;
+}
+}
+}
+}
+
+if(t==1)
+{
 shape[0].x=shape[0].x-1;
 shape[0].y=shape[0].y-2;
 
@@ -731,7 +1161,9 @@ shape[2].y=shape[2].y;
 
 shape[3].x=shape[3].x-1;
 shape[3].y=shape[3].y+1;
-
+}
+else
+version=3;
 
 
 
@@ -746,6 +1178,20 @@ public class Tetris
 {
     
     int shape_no;
+ 
+    
+    
+  
+    void clearscreen()
+    {
+        try{
+            new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor();
+        }
+        catch(Exception e)
+        {
+            
+        }
+    }
 
     
    void random_generate()
@@ -754,67 +1200,242 @@ Random rand = new Random();
  int rand_int1 = rand.nextInt(5); 
  shape_no=rand_int1;
  
-}    
+}   
+ 
     
     
 public static void main(String args[])
 {
 Tetris T=new Tetris();
+    Board b=new Board();
+    
     
  T.random_generate();
     Shapes s=new Shapes();
+    Shapes s1=new Shapes();
+    
+    
+    int over=0;
     
     if(T.shape_no==0)
     { s=new Stick();
+     s1=new Stick();
      
     }
    else if(T.shape_no==1)
-     s=new Square();
+   {s=new Square();
+    s1=new Square();
+   }
    else if(T.shape_no==2)
-         s=new L();
+   { s=new L();
+    s1=new L();
+   }
      else if(T.shape_no==3)
-         s=new T();
+     {s=new T();
+      s1=new T();
+      
+     }
      else
+     {
      s=new Shape5();
+       s1=new Shape5();   
+     }
  
     
       s.cordinates();
-     s.generate_board();
+        s1=s;
+     b.generate_board();
     s.generate_shape();
+    s1.generate_shape();
    
-        s.display();
+        b.display();
+    s.remove_prevshape();
+    s1.remove_prevshape();
     
    Scanner sc=new Scanner(System.in);
     char move;
 
     
     move=sc.next().charAt(0);
+    T.clearscreen();
+    int fixed=0;
     
-    while(move!='x')
+    while(true)
     {
-        
+     fixed=0;
+        over=0;
         if(move=='l')
+        {
+          
+            
             s.left();
+            s1=s;
+
+       s.generate_shape();
+        s1.generate_shape(); 
+        
+       b.display();
+
+            
+          s.remove_prevshape(); 
+    
+
+        if(s.check_left()==1)
+          {
+              fixed=1;
+            if(s1.update_flag()==1)
+                over=1;
+            
+    
+          }
+          else
+s1.remove_prevshape();
+            
+            
+        }
         
        else if(move=='r')
-            s.right();
+       {
+          
+           
+           
+           
+           s.right();
+           s1=s;
+ 
+       s.generate_shape();
+           s1.generate_shape();
+            b.display();
+           
+          s.remove_prevshape();  
+    
+            if(s.check_right()==1)
+           {
+             fixed=1;
+               if( s1.update_flag()==1)
+                   over=1;
+            
+
+           }
+         else
+          s1.remove_prevshape();
+           
+       }
         
         else if(move=='b')
+        {
+           
+       
             s.bottom();  
+            s1=s;
+ 
+       s.generate_shape();
+            s1.generate_shape();
+            
+               b.display();
+                s.remove_prevshape(); 
+          
+        
+  
+          
+    
+         if(s.check_bottom()==1)
+            {
+                fixed=1;
+   if( s1.update_flag()==1)
+       over=1;
+               
+            
+            }
+            else
+           s1.remove_prevshape(); 
+           
+          }
         
      else if(move=='c')       
-   s.select_version();
+     {s.select_version();
+      s1=s;
+      //s1.select_version();
+
+       s.generate_shape();
+       s1.generate_shape();
+ 
+        b.display();
+        
+       
+          s.remove_prevshape();
+       s1.remove_prevshape();
+     }
+ 
          
-          s.generate_board();
-         s.generate_shape();
-  
-        s.display();
-     
+         if(fixed==1)
+        {
+        
+       s=s1;
+             s.generate_shape();
+             b.display();
+             
+                if(over==1)
+                    break;
+             
+   T.clearscreen();
+             
+ 
+        
+          
+       s.generate_shape();
+       s1.generate_shape();
+             
+        
+T.random_generate();
+    
+    if(T.shape_no==0)
+    { s=new Stick();
+     s1=new Stick();
+    }
+   else if(T.shape_no==1)
+   { s=new Square();
+     s1=new Square();
+   }
+   else if(T.shape_no==2)
+   {s=new L();
+    s1=new L();
+   }
+     else if(T.shape_no==3)
+     {s=new T();
+      s1=new T();
+     }
+     else
+     { s=new Shape5();
+      s1=new Shape5();
+     }
+ 
+
+        s.cordinates(); 
+ 
+        s1=s; 
+ 
+         
+       s.generate_shape();
+                   s1.generate_shape();
+
+        b.display();
+              s.remove_prevshape();
+ 
+      s1.remove_prevshape();
+       
+        }
+    
        move=sc.next().charAt(0);
-         
+           T.clearscreen();
+   
+    
+            
         
        
     }
+    
+    System.out.println("GAME OVER");
 
 }
 }
